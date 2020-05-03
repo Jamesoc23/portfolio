ab4086a7d9c9bb28b7572763b39552c3a3abb88e
James OConnor <james.oc23@hotmail.com>
2020-05-03 18:30:00 +0100

    Updated deployment script

--- upload_portfolio.py
+++ upload_portfolio.py
@@ -3,17 +3,28 @@ import io
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3')
-portfolio_bucket = s3.Bucket('joconnor.io')
-build_bucket = s3.Bucket('build.joconnor.io')
-portfolio_zip = io.BytesIO()
-build_bucket.download_fileobj('portfolio_build.zip', portfolio_zip)
-
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm, 
-            ExtraArgs={
-                'ContentType': mimetypes.guess_type(nm)[0]
-            })
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+def lambda_handler(event, context):
+    
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:039741258094:deploy_portfolio')
+    try:
+        
+        s3 = boto3.resource('s3')
+        portfolio_bucket = s3.Bucket('joconnor.io')
+        build_bucket = s3.Bucket('build.joconnor.io')
+        
+        portfolio_zip = io.BytesIO()
+        build_bucket.download_fileobj('portfolio_build.zip', portfolio_zip)
+        
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm, 
+                    ExtraArgs={
+                        'ContentType': mimetypes.guess_type(nm)[0]
+                    })
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+                
+        topic.publish(Subject="Portfolio Deployment", Message="Deployment Successful!")
+    except:
+        topic.publish(Subject="Portfolio Deployment", Message="Deployment Unsuccessful :(")
