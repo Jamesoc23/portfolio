72d53100b242dea593f0643a32d0c89ca6e7f137
James OConnor <james.oc23@hotmail.com>
2020-05-04 13:17:40 +0100

    Added first react component - example-work

--- js/main.js
+++ js/main.js
@@ -1,4 +1,30 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log("Is this working?")
-console.log("I think so!")
+const myWork = [
+    {   
+        'title': 'Work Example 1',
+        'img': {
+            'src': 'images/example1.png',
+            'desc': 'example screenshot of a project involving code'
+        },
+    },
+    {   
+        'title': 'Work Example 2',
+        'img': {
+            'src': 'images/example2.png',
+            'desc': 'example screenshot of a project involving chemistry'
+        },
+    },
+    {   
+        'title': 'Work Example 3',
+        'img': {
+            'src': 'images/example3.png',
+            'desc': `“Bengal cat” by roberto shabs is licensed under CC BY 2.0
+                    https://www.flickr.com/photos/37287295@N00/2540855181`
+        },
+    }
+]
+
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
